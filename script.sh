5157f81e894636931ed5ffca32ee820b573581de
Cailey Brogan <cbrogan1@crcfe01.crc.nd.edu>
2019-10-16 11:52:45 -0400

    still not sure what is wrong here

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #usage: bash script.sh ref_seqences/mcrAgene_*.fasta ref_sequences/hsp70gene_*.fasta proteomes/*.fasta
 
 #align mcra gene
-cat $2 > masterMCRA.fasta
+cat $1 > masterMCRA.fasta
 ./muscle -in masterMCRA.fasta -out mcra_align.muscle
 
 #align hsp70 genes
-cat $3 > masterHSP.fasta
+cat $2 > masterHSP.fasta
 ./muscle -in masterHSP.fasta -out hsp_align.muscle
 
