b20f564f34e726adf7d6f3e01a6370b556cc3e74
Cailey Brogan <cbrogan1@crcfe01.crc.nd.edu>
2019-10-16 10:48:17 -0400

    second commit

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
-#usage: bash script.sh proteome/*.fasta ref_seqences/*.fasta
+#usage: bash script.sh proteome/*.fasta ref_seqences/mcrAgene_**.fasta ref_sequences/hsp70gene_*.fasta
 
+#align with mcra gene
 for file in $1
 do
 	for file in $2
@@ -8,3 +9,11 @@ do
 	done
 done
 
+#align with hsp70 gene
+for file in $1 (or already aligned file)
+do
+	for file in $3
+	do
+		#muscle command for alignment
+	done
+done
