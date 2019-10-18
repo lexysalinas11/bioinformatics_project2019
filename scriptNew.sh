00e2cb8c8cc7501a41c9f8ddc220a183c27df089
Cailey Brogan <cbrogan1@crcfe01.crc.nd.edu>
2019-10-17 20:04:45 -0400

    updated

--- scriptNew.sh
+++ scriptNew.sh
@@ -1,23 +1,29 @@
-#Usage: bash scriptNew.sh ref_sequences proteomes <hmmr path>
+#Usage: bash scriptNew.sh ref_sequences proteomes <muscle path> <hmmr path> <mcra output> <hsp output>
 
 #combine files for and align McrA gene
 cat $1/mcrAgene_* > $1/ref_mcrA.fasta
-./muscle -in $1/ref_mcrAgenes.fasta -out align_mcrA.muscle
+$3 -in $1/ref_mcrAgenes.fasta -out align_mcrA.muscle
 
 #combine files for and align hsp70 gene
 cat $1/hsp70gene_* > $1/ref_hsp70.fasta
-./muscle -in $1/ref_hsp70.fasta -out align_hsp70.muscle
+$3 -in $1/ref_hsp70.fasta -out align_hsp70.muscle
 
 #build hmmr profile
-$3/hmmbuild mcrAgene.hmm align_mcrA.muscle
-$3/hmmbuild hsp70.hmm align_hsp70.muscle
+$4/hmmbuild mcrAgene.hmm align_mcrA.muscle
+$4/hmmbuild hsp70.hmm align_hsp70.muscle
 
 #search genes for matches
 for file in $2/*
 do
-	$3/hmmsearch --tblout $file"_mcra_table" mcrAgene.hmm
-	$3/hmmsearch --tblout $file"_hsp70_table" hsp70.hmm
+	$4/hmmsearch --tblout $file"_mcra_table" mcrAgene.hmm $file
+	$4/hmmsearch --tblout $file"_hsp70_table" hsp70.hmm $file
 done
+
+#search for combined proteome sequences in hmmr profile
+$4/hmmsearch -o $5 hsp70.hmm $2/proteome_combined.fasta
+$4/hmmsearch -o $6 mcra_results.  mcrAgene.hmm $2/proteome_combined.fasta
+
+
 
 
 
